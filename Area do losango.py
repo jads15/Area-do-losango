51a60c07d44a8d68847443495d709dbccd3c86ab
João Antônio <77999403+jads15@users.noreply.github.com>
2021-02-27 00:12:07 -0300

    Update Area do losango.py

--- Area do losango.py
+++ Area do losango.py
@@ -1,4 +1,5 @@
-D =int(input("Digite a diagonal maior"))
-d =int(input("Digite a diagonal menor"))
-Area = D*d/2
-print("O valor da aréa do losango é: %s"%Area)
+
+D  = int ( input ( "Digite a diagonal maior" ))
+d  = int ( input ( "Digite a diagonal menor" ))
+Área  =  D * d / 2
+print ( "O valor da aréa do losango é:% s" % Área,"cm²" )
